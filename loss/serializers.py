b39e0b3081762466925486363d126e89d7bd15b4
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-02-24 18:33:50 +0545

    Loss api

---
+++ loss/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from .models import (
+    Loss,
+    People,
+    Family,
+    Infrastructure,
+    LivestockType,
+    Livestock,
+    InfrastructureType,
+)
+
+
+class SimpleLossSerializer(serializers.ModelSerializer):
+    people_death_count = serializers.IntegerField(
+        required=False, read_only=True
+    )
+    livestock_destroyed_count = serializers.IntegerField(
+        required=False, read_only=True
+    )
+    infrastructure_destroyed_count = serializers.IntegerField(
+        required=False, read_only=True
+    )
+
+    class Meta:
+        model = Loss
+        exclude = ('detail',)
+
+
+class PeopleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = People
+        exclude = ('name',)
+
+
+class FamilySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Family
+        exclude = ('phone_number',)
+
+
+class InfrastructureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Infrastructure
+        fields = '__all__'
+
+
+class InfrastructureTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InfrastructureType
+        fields = '__all__'
+
+
+class LivestockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Livestock
+        fields = '__all__'
+
+
+class LivestockTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LivestockType
+        fields = '__all__'
--- loss/views.py
+++ loss/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import (
+    SimpleLossSerializer,
+    InfrastructureTypeSerializer,
+    LivestockTypeSerializer
+)
+from .models import (
+    Loss,
+    InfrastructureType,
+    LivestockType
+)
+from bipad.models import DistinctSum
+from django.db.models import Q
+from django.db.models.functions import Coalesce
 
-# Create your views here.
+
+class SimpleLossViewSet(viewsets.ModelViewSet):
+    serializer_class = SimpleLossSerializer
+    queryset = Loss.objects.annotate(
+        people_death_count=Coalesce(DistinctSum(
+            'peoples__count',
+            filter=Q(peoples__status='dead')
+        ), 0),
+        livestock_destroyed_count=Coalesce(DistinctSum(
+            'livestocks__count',
+            filter=Q(livestocks__status='destroyed')
+        ), 0),
+        infrastructure_destroyed_count=Coalesce(DistinctSum(
+            'infrastructures__count',
+            filter=Q(infrastructures__status='destroyed')
+        ), 0),
+    )
+
+
+class InfrastructureTypeViewSet(viewsets.ModelViewSet):
+    serializer_class = InfrastructureTypeSerializer
+    queryset = InfrastructureType.objects.all()
+
+
+class LivestockTypeViewSet(viewsets.ModelViewSet):
+    serializer_class = LivestockTypeSerializer
+    queryset = LivestockType.objects.all()
