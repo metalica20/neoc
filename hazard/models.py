aa1bde1aa3e13c87f976b88fab46e1d3342a8e40
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-02-25 11:13:23 +0545

    Alter icon field in hazard

--- hazard/models.py
+++ hazard/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.postgres.fields import JSONField
 
 
 class Hazard(models.Model):
     title = models.CharField(max_length=250, unique=True)
     description = models.TextField(default=None, null=True, blank=True)
-    icon = models.FileField(
-        upload_to='hazard-icons/',
-        default=None, null=True, blank=True
-    )
-    style = JSONField(default=None, null=True, blank=True)
+    icon = models.CharField(max_length=25, default=None, null=True, blank=True)
     resources = models.ManyToManyField(ContentType, through="HazardResources")
 
     def __str__(self):
