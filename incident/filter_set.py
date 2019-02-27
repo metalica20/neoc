dfc7e09e4405bf185409c87fb4148cf038f95c78
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-02-27 15:16:34 +0545

    Incident response

--- incident/filter_set.py
+++ incident/filter_set.py
@@ -1,4 +1,5 @@
 import django_filters
+from hazard.models import Hazard
 from .models import Incident
 
 
@@ -11,7 +12,12 @@ class IncidentFilter(django_filters.FilterSet):
         field_name='incident_on',
         lookup_expr='lte',
     )
+    hazard = django_filters.ModelMultipleChoiceFilter(
+        queryset=Hazard.objects.all(),
+        lookup_expr='in',
+        widget=django_filters.widgets.CSVWidget,
+    )
 
     class Meta:
         model = Incident
-        fields = ['event', 'hazard']
+        fields = ['event', ]
--- resources/serializers.py
+++ resources/serializers.py
@@ -1,15 +1,19 @@
-from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 from .models import Resource
 
 
 class ResourceSerializer(serializers.ModelSerializer):
+    distance = serializers.SerializerMethodField()
     resource_type = serializers.SlugRelatedField(
         read_only=True,
         slug_field='model',
         source='polymorphic_ctype',
     )
 
+    def get_distance(self, obj):
+        return int(obj.distance.m)
+
     class Meta:
         model = Resource
-        fields = ('title', 'description', 'point', 'ward', 'resource_type')
+        fields = ('title', 'description', 'point',
+                  'ward', 'resource_type', 'distance')
