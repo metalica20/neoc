a3d9ab1026e648b1582b2992c81fabb14f9a9111
sumit <rounnn8@gmail.com>
2019-03-05 15:06:59 +0545

    geo

---
+++ risk_profile/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('hospital',views.HospitalViewSet.as_view({'get':'list'}),name='hospital'),
+    path('hospitaljson',views.HospitalGeojsonViewSet.as_view(),name='hospitaljson'),
+]
