4ae8310dd7abff8107acb1189d84e181ef824071
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-03-07 16:53:18 +0545

    button

--- risk_profile/urls.py
+++ risk_profile/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('layer',views.LayerViewset.as_view({'get':'list'}),name='layer'),
-    path('hospitaljson',views.HospitalGeojsonViewSet.as_view(),name='hospitaljson'),
-    path('marketcenterjson',views.MarketCenterGeojsonViewSet.as_view(),name='marketcenterjson'),
+    path('Hospital',views.HospitalGeojsonViewSet.as_view(),name='hospitaljson'),
+    path('MarketCenter',views.MarketCenterGeojsonViewSet.as_view(),name='marketcenterjson'),
     # path('hospital',views.LayerViewset.as_view({'get':'list'}),name='hospital'),
 ]
