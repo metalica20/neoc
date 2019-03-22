2374bb23c2effc1f655dce112f581afe2ccc00b8
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-03-22 22:14:20 +0545

    Extra attributes in resource api and optimize apis

--- incident/views.py
+++ incident/views.py
@@ -1,10 +1,16 @@
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Prefetch
-from rest_flex_fields import FlexFieldsModelViewSet, is_expanded
+from rest_flex_fields import (
+        FlexFieldsModelViewSet,
+        is_expanded,
+        )
 from .serializers import IncidentSerializer
 from resources.models import Resource
-from resources.serializers import ResponseSerializer
+from resources.serializers import (
+        ResponseSerializer,
+        DetailResponseSerializer,
+        )
 from .models import Incident
 from .filter_set import IncidentFilter
 from loss.models import Loss
@@ -31,21 +37,20 @@ class IncidentViewSet(FlexFieldsModelViewSet):
     ]
 
     def get_queryset(self):
-        queryset = Incident.objects.filter(verified=True)
+        # TODO: research why wards are queried by default
+        queryset = Incident.objects.filter(verified=True).prefetch_related('wards')
         loss_queryset = Loss.with_stat.all()
         if is_expanded(self.request, 'event'):
             queryset = queryset.select_related('event')
         if is_expanded(self.request, 'hazard'):
             queryset = queryset.select_related('hazard')
-        if is_expanded(self.request, 'wards'):
-            queryset = queryset.prefetch_related('wards')
-        if is_expanded(self.request, 'loss.peoples'):
+        if is_expanded(self.request, 'peoples'):
             loss_queryset = loss_queryset.prefetch_related('peoples')
-        if is_expanded(self.request, 'loss.families'):
+        if is_expanded(self.request, 'families'):
             loss_queryset = loss_queryset.prefetch_related('families')
-        if is_expanded(self.request, 'loss.livestocks'):
+        if is_expanded(self.request, 'livestocks'):
             loss_queryset = loss_queryset.prefetch_related('livestocks')
-        if is_expanded(self.request, 'loss.infrastructures'):
+        if is_expanded(self.request, 'infrastructures'):
             loss_queryset = loss_queryset.prefetch_related('infrastructures')
         if is_expanded(self.request, 'loss'):
             queryset = queryset.prefetch_related(Prefetch(
@@ -69,7 +74,11 @@ class IncidentViewSet(FlexFieldsModelViewSet):
                 ))\
                 .annotate(
                     distance=Distance("point", location)
-            ).order_by('distance')
+            ).select_related('polymorphic_ctype').order_by('distance')
 
-        serializer = ResponseSerializer(resources, many=True)
+        meta = self.request.query_params.get('meta')
+        if meta:
+            serializer = DetailResponseSerializer(resources, many=True)
+        else:
+            serializer = ResponseSerializer(resources, many=True)
         return Response(serializer.data)
