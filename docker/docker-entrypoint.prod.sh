5ae2eeddfd99e202378dc4e02c90748a6768be3a
Bishal Timilsina <bishaltimilsina@gmail.com>
2019-03-28 11:08:09 +0545

    Use pypy

--- docker/docker-entrypoint.prod.sh
+++ docker/docker-entrypoint.prod.sh
@@ -1,5 +1,5 @@
-python manage.py collectstatic --no-input
-python manage.py migrate --no-input
+pypy3 manage.py collectstatic --no-input
+pypy3 manage.py migrate --no-input
 celery -A bipad beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 celery -A bipad worker --concurrency=4 -l info &
 gunicorn --bind 0.0.0.0:8000 --workers=5 bipad.wsgi:application
