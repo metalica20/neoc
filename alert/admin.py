0dc95ba3e476cef04ffc342a623c729a5d9cadf8
Dipkiran <071bct516dip@pcampus.edu.np>
2019-04-01 15:02:10 +0545

    Auto generate alert for earthquake

--- alert/admin.py
+++ alert/admin.py
@@ -1,8 +1,22 @@
-from django.contrib import admin
+from django.contrib import (
+    admin,
+    messages,
+)
 from bipad.admin import GeoModelAdmin
 from .models import Alert
+from .utils import get_similar_alerts
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Alert)
 class AlertAdmin(GeoModelAdmin):
     exclude = ('wards',)
+
+    def save_model(self, request, obj, form, change):
+        super(AlertAdmin, self).save_model(request, obj, form, change)
+        similar_alerts = get_similar_alerts(obj)
+        for alert in similar_alerts:
+            messages.add_message(request, messages.INFO, mark_safe(
+                "Similar alert <a href='/admin/alert/alert/%d/change/'>%s</a> already exists"
+                % (alert.id, alert.title)
+            ))
