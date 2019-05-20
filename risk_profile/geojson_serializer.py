ac6a260ef3ecc7c6373904e4c40e85a00fb0f00d
sumit <rounnn8@gmail.com>
2019-05-20 18:22:11 +0545

    postgres view and hydropower model added

--- risk_profile/geojson_serializer.py
+++ risk_profile/geojson_serializer.py
@@ -6,7 +6,7 @@ class Serializer(GeoJSONSerializer):
     def get_dump_object(self, obj):
         data = super(Serializer, self).get_dump_object(obj)
         # print(data)
-        print(obj.title)
+        # print(obj.title)
         # Extend to your taste 'ward': obj.ward.title
         data['properties'].update({'title':obj.title,'description': obj.description,})
         data.update({'geometry':{"type": "Point", "coordinates":[obj.point.x,obj.point.y]}})
